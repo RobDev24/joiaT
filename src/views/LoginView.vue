3b1045cdfc1317376f55ef1a68eb1e9ec9309bcf
Robson A <robson_rodrigues_alves@hotmail.com>
2024-08-28 18:10:59 -0300

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,59 +1,80 @@
 <template>
     <div class="container">
+        
+        <div class="card">
 
-    <nav class="login">
+            <h2>Login</h2>
 
-        <form action="" method="post">
+            <div class="label">
+               
+                <label for="usuario">Usuário</label>
+                <input type="text" id="usuario" placeholder="e-mail">
 
-            <label for="">Usuário</label>
-            <input type="text">
+            </div>
 
-            <label for="">Senha</label>
-            <input type="password">
+            <div class="label">
+               
+               <label for="senha">Senha</label>
+               <input type="password" id="senha" placeholder="senha">
 
+           </div>
+
+           <div>
             <button>Entrar</button>
-        </form>
-    </nav>
-    <div class="suporte">
-<h2>suporte</h2>
+           </div>
+
+           <div> <hr></div>
+
+           <p>Não tem uma conta? <a href="">Cadastre-se</a> </p>
+        </div>
+
     </div>
-</div>
 </template>
 
+
 <style scoped>
+*{
+    margin: 0;
+    padding: 0;
+}
 
 .container{
-    display: block;
-    height: 61vh;
-    border: 2px solid rebeccapurple;
-    background-image: url('../assets/imagens/fundodiamantes.png');
-    background-position: center;
-    background-size: cover;
+    display: flex;
+    
+    height: 63vh;
+    
 }
-.login{
-    display: inline;
-    float: left;
-    margin-left: 3rem;
-    height: 100%;
-    width: 25rem;
-    border: 2px solid rebeccapurple;
-    background: #746d6d;
+.card{
+    background-color: rgb(72, 70, 60);
+    border: 2px solid rgb(42, 192, 12);
+    width: 20vw;
+    height: 83%;
+    padding: 20px;
+    margin-top: 10px;
+    margin-left: 5rem;
     color: aliceblue;
 }
 
-form{
-
-}
-.suporte{
-    display: inline;
-    float: right;
-    margin-right: 2rem;
-    height: 100%;
-    width: 120vh;
-    border: 2px solid rebeccapurple;
-    color: aliceblue;
+h2{
+    text-align: center;
+    margin-bottom: 20px;
 }
 
-
+input{
+    width: 90%;
+    background: transparent;
+}
+label{
+  display: flex;
+  margin-left: 10px;
+  padding: 5px;
+  margin-top: 8px;
+}
+button{
+    display: flex;
+    margin-left: 10px;
+    margin-top: 10px;
+    margin-bottom: 5px;
+    padding: 3px;
+}
 </style>
-
