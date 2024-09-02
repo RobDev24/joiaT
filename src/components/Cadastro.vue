a8f702c9a299b3c85083e0e17c2cd683d9029f1e
Robson A <robson_rodrigues_alves@hotmail.com>
2024-09-02 17:31:21 -0300

    Tela-Cadastro

--- src/components/Cadastro.vue
+++ src/components/Cadastro.vue
@@ -1,70 +1,57 @@
 <template>
     <div class="cadastro">
-        <form action="" method="post" class="formulari-cadastro">
-        <h3>Formulário de Casastro</h3>
-        <p>Todos os campos são obrigatórios</p>
+                
         
-        <div class="dados">
+            <form action="" method="post" class="formulario-cadastro">
+
+                <h3>Formulário de Casastro</h3>
+                <p>Todos os campos são obrigatórios</p>
+                <div class="dados-cad">
             <label for="nome">Nome Completo</label>
-            <input type="text" placeholder="Nome completo" autocomplete="off" required minlength="8">
+            <input type="text" class="nome" placeholder="Nome completo" autocomplete="off" required minlength="8">
 
-            <label for="date">Data de Nascimento</label>
-            <input type="date" placeholder="data de nascimento" autocomplete="off">
+            <label for="date" class="date-nasc" >Data de Nascimento</label>
+            <input type="date" class="data-nasc" placeholder="data de nascimento" autocomplete="off" required>
 
-            <div class="row">
-                <div class="input-field col m6">
+            <label for="mail">E-mail</label>
+            <input type="email" class="mail" placeholder="E-mail" autocomplete="off" required>
+
+           
             <label for="txtCep">Cep</label>
-            <input id="txtCep" type="text 
-            " name="cep" autocomplete="off" required>
-        </div>
-        </div>
-
-        <div class="row">
-                <div class="input-field col m6">
-            <label for="txtRua">Rua, Av, Alameda, travessa</label>
+            <input id="txtCep" type="text
+            " class="cep" autocomplete="off" required>
+               
+            <label for="txtRua">Largadouro</label>
             <input id="txtRua" type="text 
-            " name="Rua">
-        </div>
-        </div>
-
-        <div class="row">
-                <div class="input-field col m6">
+            " class="rua" name="Rua">
+       
             <label for="txtNumero">Numero</label>
             <input id="txtNumero" type="text 
-            " name="Numero">
-        </div>
-        </div>
-
-        <div class="row">
-                <div class="input-field col m6">
+            " class="numero" name="Numero">
+        
+    
             <label for="txtBairro">Bairro</label>
             <input id="txtBairro" type="text 
-            " name="Bairro">
-        </div>
-        </div>
+            " class="bairro" name="Bairro">
 
-        <div class="row">
-                <div class="input-field col m6">
             <label for="txtCidade">Cidade</label>
             <input id="txtCidade" type="text 
-            " name="cidade">
-        </div>
-        </div>
-
-        <div class="row">
-                <div class="input-field col m6">
+            " class="cidade" name="cidade">
+              
             <label for="txtEstado">Estado</label>
             <input id="txtEstado" type="text 
-            " name="Estado">
+            " class="estado" name="Estado">
+             
+            <button class="btn-cadastro">Cadastrar</button>
+            <p class="btn-login">Já tem cadatro? Faça o seu <router-link to="/log" class="route-login">Login</router-link></p>
         </div>
-        </div>
-        </div>
-
-
-        <button>Cadastrar</button>
-        <button><router-link to="/log">Login</router-link></button>    
-    </form>
+     </form>
+       
+       
+          
+   
 </div>
+
 </template>
 
 <script>
@@ -110,13 +97,68 @@ window.onload = function(){
     display: block;
     height: 63vh;
     border: 2px solid #fff;
+    margin-top: -4px;
 }
-.formulari-cadastro{
+.dados-cad {
+    display: flexbox;
+    width: 60vw;
+    height: 51vh;
+    margin-top: -16px;
+   overflow: auto;
+    border: 2px solid red;
+
+}
+ .cep, .numero{
+    height: 30px;
+    margin-left: -33rem;
+}
+.data-nasc{
+    margin-left: -37rem;
+}
+
+  .nome, .mail, .rua, .bairro, .cidade, .estado{
+   display: block;
+   width: 35rem;
+   height: 30px;
+   margin-left: 2rem;
+}
+
+
+label{
+    display: flex;
+    margin-top: 10px;
+    margin-left: 33px;
+}
+.date-nasc{
+    margin-left: 35px;
+}
+.formulario-cadastro{
     display: block;
     width: 60vw;
     height: 62vh;
+    margin-left: 20rem;
     background-color: #fff;
 
     border: 2px solid yellow;
+}
+
+.btn-cadastro{
+    display: inline;
+    float: left;
+    margin-top: 1em;
+    margin-bottom: 0.5rem;    
+    margin-left: 2rem;
+
+    border: 2px solid red;
+}
+
+.btn-login{
+    display: flex;
+    margin-top: 1.5rem;
+    margin-left: 20rem;
+}
+.route-login{
+margin-left: 8px;
+
 }
 </style>
